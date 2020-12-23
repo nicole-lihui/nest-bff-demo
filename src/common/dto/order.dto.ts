91b4c710d3e85f43e744f13dee87ada5f663a9a3
nicole-lihui <lihui01@hpcang.com>
2020-12-23 17:44:30 +0800

    update
    Signed-off-by: nicole-lihui <lihui01@hpcang.com>

--- src/common/dto/order.dto.ts
+++ src/common/dto/order.dto.ts
@@ -1,9 +1,16 @@
+import { IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
 class OrderDTO {
   id: string;
   count: number;
 }
 
 class FindOneOrderReq {
+  @ApiProperty({
+    required: true,
+  })
+  @IsNotEmpty({ message: '订单号不能为空' })
   id: string;
 }
 
--- src/logical/order/order.controller.ts
+++ src/logical/order/order.controller.ts
@@ -1,13 +1,10 @@
-import {
-  Controller,
-  Post,
-  UsePipes,
-  ValidationPipe,
-  Body,
-} from '@nestjs/common';
+import { Controller, Post, UsePipes, Body } from '@nestjs/common';
 import { FindOneOrderReq } from '../../common/dto/order.dto';
 import { OrderService } from './order.service';
+import { ApiTags } from '@nestjs/swagger';
+import { ValidationPipe } from '../../common/pipe/validation.pipe';
 
+@ApiTags('order')
 @Controller('order')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
